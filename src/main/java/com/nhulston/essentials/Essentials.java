d1531ad73b9b0937efd1ce9961c67cf0daa98b84
Nicholas Hulston <nicholashulston@gmail.com>
2026-01-14 22:33:26 -0500

    bump version to 1.3.0

--- src/main/java/com/nhulston/essentials/Essentials.java
+++ src/main/java/com/nhulston/essentials/Essentials.java
@@ -41,7 +41,7 @@ import com.nhulston.essentials.util.VersionChecker;
 import javax.annotation.Nonnull;
 
 public class Essentials extends JavaPlugin {
-    public static final String VERSION = "1.2.1";
+    public static final String VERSION = "1.3.0";
     
     private ConfigManager configManager;
     private StorageManager storageManager;
